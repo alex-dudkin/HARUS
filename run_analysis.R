f755531294a1dd7ee69277b9c3c1bd6ddfd16391
Alex Dudkin <alex_dudkin@yahoo.com>
2015-07-13 21:37:53 +0300

    modified:   run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,34 @@
-#test
+g_datadir <- "UCI HAR Dataset"
+g_dirs2merge <- c("test","train")
+g_mergedir <- "merged"
+#testdir <- "UCI HAR Dataset/test"
+#traindir <- "UCI HAR Dataset/train"
+#inertdir <- "Inertial Signals"
+
+mergeFiles <- function(file1, file2) {
+        f1 <- read.table(file1, stringsAsFactors = FALSE)
+        f2 <- read.table(file2, stringsAsFactors = FALSE)
+        rbind(f1, f2)
+}
+
+mergeDatasets <- function(mergedir=g_mergedir, datadir=g_datadir, dirs2merge=g_dirs2merge) {  
+        dir1 <- paste0(datadir, "/", dirs2merge[1])
+        dir2 <- paste0(datadir, "/", dirs2merge[2])
+        
+        for (d1 in list.dirs(dir1, full.names = FALSE, recursive = TRUE)) {
+                dir.create(paste0(mergedir,"/",d1), recursive = TRUE, showWarnings = FALSE)
+        }
+        
+        for (f1 in list.files(dir1, full.names = FALSE, recursive = TRUE)) { 
+                f2 <- sub(paste0(dirs2merge[1],"\\.txt$"), paste0(dirs2merge[2],".txt"), f1) 
+                f3 <- sub(paste0(dirs2merge[1],"\\.txt$"), paste0(mergedir,".txt"), f1) # get result (merged) file name 
+                
+                dt3 <- mergeFiles(paste0(dir1, "/", f1), paste0(dir2, "/", f2))
+                #print(paste0(mergedir,"/", f3))
+                write.table(dt3, file = paste0(mergedir,"/", f3), row.names = FALSE, col.names = FALSE)
+        }
+        
+        
+}  
+
+
